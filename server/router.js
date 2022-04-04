bb7d463e06e7e31bb749a7dfbba0080d3720d5a7
McCray <tubach75@hotmail.com>
2022-04-04 10:14:57 -0400

    added controllers folder and authentication file

--- server/router.js
+++ server/router.js
@@ -1,3 +1,5 @@
+const Authentication = require('./controllers/authentication')
+
 module.exports = function(app){
     app.get('/', function(req, res, next) {
         res.send(['notebooks', 'phone', 'clock radio', 'napkins'])
