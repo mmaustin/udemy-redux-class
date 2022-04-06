8892c7f043d941acf773a44f52ef52865d217dac
McCray <tubach75@hotmail.com>
2022-04-06 09:12:33 -0400

    implemented ability to save users in authentication file

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -5,13 +5,21 @@ exports.signup = function(req, res, next) {
     const email = req.body.email
     const password = req.body.email
     //see if a user with the given email exists
-    User.findOne({email: email}, ()=>{err, existingUser})
-
-    //return error, if a user with email does exist
-
-
-    //if a user with email does Not exist, create and save user record
-
-
-    //respond to request indicating the user was created
+    User.findOne({email: email}, (err, existingUser)=>{
+        if (err) {return next(err);}
+        //return error, if a user with email does exist
+        if (existingUser){
+            return res.status(422).send({error: 'Email is in use'})
+        }
+        //if a user with email does Not exist, create and save user record
+        const user = new User({
+            email: email,
+            password: password
+        })
+        user.save((err)=> {
+            if (err) {return next(err);}
+        //respond to request indicating the user was created
+        res.json(user);
+        });
+    })
 }
