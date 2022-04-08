d3a93303ce59dcd2a0eab8e27052099ab91c1525
McCray <tubach75@hotmail.com>
2022-04-08 09:39:29 -0400

    added comments to the bcrypt salting process functions

--- server/router.js
+++ server/router.js
@@ -3,9 +3,3 @@ const Authentication = require('./controllers/authentication')
 module.exports = function(app){
     app.post('/signup', Authentication.signup);
 }
-
-/*module.exports = function(app){
-    app.get('/', function(req, res, next){
-        res.send(['annoyed', 'and', 'frustrated'])
-    })
-}*/
