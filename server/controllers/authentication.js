e0882c89d92fe940a4019a1b6b26a530b4966a42
McCray <tubach75@hotmail.com>
2022-04-08 10:21:14 -0400

    imported jwt and config and implemented a token for user function

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
-const User = require('../models/user')
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config')
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     //req.body means anything contained within the post request
